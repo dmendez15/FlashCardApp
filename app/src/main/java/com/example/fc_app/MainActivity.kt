4dd1bcc54c8549adbdfe7b7d484667d75e9dde64
David Mendez <dmendez15@islander.tamucc.edu>
2021-03-26 22:30:05 -0500

    Added a test text view

--- app/src/main/java/com/example/fc_app/MainActivity.kt
+++ app/src/main/java/com/example/fc_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.fc_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -16,17 +17,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun showNewSetTextDialog(){
 
+        val textView = findViewById<TextView>(R.id.textView)
        val floatingAB = findViewById<FloatingActionButton>(R.id.floatingActionButton)
 
         floatingAB.setOnClickListener {
             val builder = AlertDialog.Builder(this)
             val inflater = layoutInflater
             val dialogLayout = inflater.inflate(R.layout.new_set_layout,null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.et_setName)
 
             with(builder){
                 setTitle("Create new set")
                 setPositiveButton("Save"){dialog, which ->
                     //Add new card to home screen
+                    textView.text = editText.text.toString()
                 }
                 setNegativeButton("Cancel"){dialog, which ->
                     //Cancel
